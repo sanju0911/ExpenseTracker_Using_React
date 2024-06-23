90a26543cf2565e342e1a8cf0301345262cc1e1a
sanju <sanjayjanardhan231@gmail.com>
2024-06-23 14:54:40 +0530

    added props
    added dynamic expenses using props

--- learning_react/src/App.js
+++ learning_react/src/App.js
@@ -1,10 +1,35 @@
+// Write your code at relevant places in the code below:
+
 import ExpenseItem from "./components/ExpenseItem";
 
 function App() {
   return (
-    <div className="App">
-      <h1>data</h1>
-      <ExpenseItem></ExpenseItem>
+    <div>
+      <h1>Let's get Started</h1>
+      <ExpenseItem
+        date={new Date(2023, 7, 15)}
+        location="Bangalore"
+        title="Insurance"
+        price="50"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 3, 25)}
+        location="Delhi"
+        title="Book"
+        price="20"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 10, 11)}
+        location="Hyderabad"
+        title="Pen"
+        price="5"
+      ></ExpenseItem>
+      <ExpenseItem
+        date={new Date(2023, 1, 14)}
+        location="Mumbai"
+        title="Laptop"
+        price="200"
+      ></ExpenseItem>
     </div>
   );
 }
