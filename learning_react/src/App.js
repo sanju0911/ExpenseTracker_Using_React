6d6cf1ceff611893157de4ca68c1e09f3a0e5729
sanju <sanjayjanardhan231@gmail.com>
2024-06-23 11:49:49 +0530

    added Custom Components
    added ExpenseItems

---
+++ learning_react/src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is expense item</h1>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App;
