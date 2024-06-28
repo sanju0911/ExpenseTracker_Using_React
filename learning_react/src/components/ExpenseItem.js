955c2df846e79496a2fc297e7515d460a302fa35
sanju <sanjayjanardhan231@gmail.com>
2024-06-28 11:45:52 +0530

    added usestate
    added for change title

--- learning_react/src/components/ExpenseItem.js
+++ learning_react/src/components/ExpenseItem.js
@@ -3,17 +3,23 @@
 
 import "./ExpenseItem.css";
 import Card from "./Card";
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.title);
+  function ButtonClicked(event) {
+    setTitle("New Title");
+  }
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.price}</div>
       </div>
+      <button onClick={ButtonClicked}> change title</button>
     </Card>
   );
 }
