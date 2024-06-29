48770d0d26fbfb2ce4c91d125c87cc752926a2b4
sanju <sanjayjanardhan231@gmail.com>
2024-06-29 13:00:39 +0530

    added chartbars

--- learning_react/src/components/Expensesdata/Expenses.js
+++ learning_react/src/components/Expensesdata/Expenses.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
+import Chart from "../Chart/Chart";
 import "./Expenses.css";
 import Card from "../UI/Card";
 
@@ -15,12 +16,33 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
+  const chartDataPoints = [
+    { label: "Jan", value: 0 },
+    { label: "Feb", value: 0 },
+    { label: "Mar", value: 0 },
+    { label: "Apr", value: 0 },
+    { label: "May", value: 0 },
+    { label: "Jun", value: 0 },
+    { label: "Jul", value: 0 },
+    { label: "Aug", value: 0 },
+    { label: "Sep", value: 0 },
+    { label: "Oct", value: 0 },
+    { label: "Nov", value: 0 },
+    { label: "Dec", value: 0 },
+  ];
+
+  for (const expense of filteredExpenses) {
+    const expenseMonth = expense.date.getMonth(); // starting at 0 => January => 0
+    chartDataPoints[expenseMonth].value += expense.price;
+  }
+
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={changeFilterHandler}
       />
+      <Chart dataPoints={chartDataPoints} />
       <ExpensesList items={filteredExpenses} />
     </Card>
   );
